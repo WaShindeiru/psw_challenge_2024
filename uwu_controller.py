41a0846ced29eaa75e8795eefa03991ab84f22bd
Franciszek <franekdata@gmail.com>
2024-12-02 15:20:36 +0100

    Update uwu_controller.py

--- uwu_controller.py
+++ uwu_controller.py
@@ -13,6 +13,12 @@ class State(Enum):
     FlyThrough = 2
     Wait = 3
 
+def quaternion2euler(quaternion_rot_vector):
+    qx, qy, qz, qw = quaternion_rot_vector
+    rx = np.arctan2(2*(qw*qx + qy*qz), 1-2*(qx**2+qy**2))
+    ry = -(np.pi/2) + 2*np.arctan2(np.sqrt(1+2*(qw*qy-qx*qz)), np.sqrt(1-2*(qw*qy-qx*qz)))
+    rz = np.arctan2(2*(qw*qz + qx*qy), 1-2*(qy**2+qz**2))
+    return (rx, ry, rz)
 
 
 
@@ -38,25 +44,54 @@ if __name__ == '__main__':
         print(f"Started with state: {current_state}")
         while not rospy.is_shutdown():
             if current_state == State.FlyZero:
-                controller.fly_to([-2, -2, 2.8], [0, 0, 2.8])
+                controller.fly_to([-2, -2, 2.5], [0, 0, 0])
 
                 if controller.in_destination and controller.finished_rotating:
                     current_state = State(current_state.value + 1)
 
             if current_state == State.FlyAruco:
-                x_diff = 2.8
+                x_diff = -0.4
                 if cam_sub.img is not None:
                     arucoDetection.detect(cam_sub.img)
                     result = arucoDetection.markers.get(current_marker, None)
+                    next_aruco = arucoDetection.markers.get(current_marker+1, None)
                     print(f"markers: {arucoDetection.markers.keys()} current_marker: {current_marker}, state: {current_state}")
+                    next_aruco_direction = None
                     if result is not None:
                         x, y = result
-                        x_diff = ((640 - x) * 0.05 ) *(np.pi/180)
+                        x_diff = ((640 - x) * 0.05 ) * (np.pi/180)
+                        #print(f"x_diff: {x_diff}")
+                        if next_aruco is not None:
+                            x_next, y_next = next_aruco
+                            x_diff_next = 640 - x_next
+                            next_aruco_direction = -1 if x_diff_next > 0 else 1   # -1 is left, 1 is right
+                    #else:
+                        
+                        # if next_aruco_direction is not None:
+                        #     print('test')
+                        #     x_diff = 0.2 * next_aruco_direction
+                        #else:
+                        #    x_diff = 0
 
-                direction = [0 , 0, 2.8]
+                direction = [0, 0, 2.5]
+                print(f'Rotation: {x_diff}')
                 controller.fly_local(direction, x_diff)
 
-                if x_diff< 0.005:
+                if np.abs(x_diff) < 0.05:
+                    #print("Finished calibration")
+                    current_quaternion = controller.pos_sub.get_rot_quaternion()
+                    current_euler_z = quaternion2euler(current_quaternion)[2]
+                    #print(f'Yaw: {current_euler_z / np.pi * 180.0}')
+                    x_new ,y_new, _ = controller.pos_sub.get_pos()
+                    # a = np.tan(current_euler_z)
+                    # b = current_point[1] - a * current_point[0]
+                    length = 5
+                    y_new += np.sin(current_euler_z) * length   #current_point[0] + 5
+                    x_new += np.cos(current_euler_z) * length   #y_new = a * x_new + b
+
+                    new_point = [x_new, y_new, 2.5]
+                    #print(new_point)
+                    cnt = 0
                     current_state = State(current_state.value + 1)
 
             if current_state == State.FlyThrough:
@@ -67,18 +102,25 @@ if __name__ == '__main__':
 
 
                     if result is not None:
-                        direction = [0.3 , 0, 2.8]
-                        controller.fly_local(direction, 0)
+                        controller.fly_to(new_point, current_euler_z, k=0.7) #fly_local(direction, 0)
                     else:
-                        current_marker += 1
-                        current_state = State.FlyAruco
+                        cnt += 1
+                        
+                        if cnt > 2:
+                            current_marker += 1
+                            if current_marker > 5:
+                                current_state = State.Wait
+                                print("Finished flying through")
+                            else:
+                                current_state = State.FlyAruco
                 
 
             if current_state == State.Wait:
-                direction = [0.3 , 0, 2.8]
+                arucoDetection.detect(cam_sub.img)
+                direction = [-5 , 0, 2.8]
                 controller.fly_local(direction, 0)
-                time.sleep(2)
-                current_state = State.FlyAruco
+                # time.sleep(2)
+                # current_state = State.FlyAruco
 
             #if prev_state != current_state:
             #print(f"current state {current_state}")
